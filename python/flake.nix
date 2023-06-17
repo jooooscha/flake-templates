ad9c08e736051fe0469a007231347dc2233c8932
Joscha Loos <j.loos@posteo.net>
2023-06-17 22:33:00 +0200

    add python flake

---
+++ python/flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+        pkgs = import nixpkgs { inherit system; };
+
+        packaged-python = pkgs.python3.withPackages (p: with p; [
+          # Examples
+          # pyyaml
+          # matplotlib
+          # numpy
+          # termcolor
+          # configargparse
+          # (
+            # buildPythonPackage rec {
+              # pname = "deserialize";
+              # version = "1.8.3";
+              # src = fetchPypi {
+                # inherit pname version;
+                # sha256 = "sha256-0aozmQ4Eb5zL4rtNHSFjEynfObUkYlid1PgMDVmRkwY=";
+              # };
+              # doCheck = false;
+            # }
+          # )
+        ]);
+
+      in {
+        packages = { };
+        apps = { };
+
+        devShells.default =
+          pkgs.mkShell {
+            name = "Paper Tracker Shell";
+            packages = [
+              packaged-python
+            ];
+          };
+      }
+    );
+}
