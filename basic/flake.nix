3e057b318afe7c4ee3dfe0b643f8dd081cb67645
Joscha Loos <j.loos@posteo.net>
2023-06-17 22:33:26 +0200

    feat(basic): flake-utils follows nixpkgs

--- basic/flake.nix
+++ basic/flake.nix
@@ -1,7 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
