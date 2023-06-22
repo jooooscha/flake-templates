aa311b19e7dda87754199da166094b0389f46b5e
Joscha Loos <j.loos@posteo.net>
2023-06-22 15:13:27 +0200

    fix python

--- python/flake.nix
+++ python/flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:nixos/nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
