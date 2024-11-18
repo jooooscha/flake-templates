21916f490491e9d04d0f14fd14c940ef8a0fd0f4
Joscha Loos <j.loos@posteo.net>
2024-11-18 16:43:30 +0100

    feat: improve basic

--- basic/flake.nix
+++ basic/flake.nix
@@ -8,7 +8,10 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
       in {
         packages = { };
         apps = { };
